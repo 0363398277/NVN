e5af1e5c22faa0924f732d518a7100464174a42a
0363398277 <1995nguyenvannhat1995@gmail.com>
2026-08-05 19:46:51 +0700

    Add files via upload

---
+++ Catlieu.js
@@ -0,0 +1,223 @@
+const data = [
+{
+  "group": "Người lớn",
+  "title": "Cảm ho đờm",
+  "lessons": [
+    "Kháng sinh",
+    "Kháng viêm",
+	"Hạ sốt, giảm đau",
+    "Giảm ho đờm",	
+    "Vitamin (3B, C)"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Đau rát họng có nhiễm khuẩn",
+  "lessons": [
+    "Kháng sinh",
+    "Kháng viêm",
+	"Hạ sốt, giảm đau",		
+    "Giảm ho Eugica",
+    "Vitamin (3B, C)"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Viêm xoang",
+  "lessons": [
+    "Kháng sinh",
+    "Kháng viêm",
+	"Hạ sốt, giảm đau",		
+    "Kháng H1",
+    "Vitamin (3B, C)"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Cảm ho khan có nhiễm khuẩn",
+  "lessons": [
+    "Kháng sinh",
+    "Kháng viêm",
+	"Cảm - hạ sốt- giảm đau",
+    "Vitamin (3B, C)"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Nhiễm trùng tiểu",
+  "lessons": [
+    "Kháng sinh",
+    "Nhiễm trùng tiểu",
+	"Giảm đau cơ trơn",
+    "Vitamin (3B, C)"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Đau răng",
+  "lessons": [
+    "Kháng sinh",
+    "Kháng viêm",
+	"Hạ sốt, giảm đau",
+    "Vitamin (3B, C)"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Đau mắt",
+  "lessons": [
+    "Kháng sinh",
+    "Kháng viêm",
+	"Hạ sốt, giảm đau",
+    "Kháng H1"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "U nhọt",
+  "lessons": [
+    "Kháng sinh",
+    "Kháng viêm",
+	"Hạ sốt, giảm đau",
+    "Vitamin (3B, C)"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Vết thương ngoài da",
+  "lessons": [
+    "Kháng sinh",
+    "Kháng viêm dạng men",
+	"Hạ sốt, giảm đau",
+    "Vitamin (3B, C)"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Nhiệt miệng",
+  "lessons": [
+    "Kháng viêm dạng men",
+	"Hạ sốt, giảm đau",
+    "Vitamin (3B, C)",
+	"Khoáng chất (kẽm)"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Viêm họng thông thường",
+  "lessons": [
+    "Kháng viêm",
+	"Hạ sốt, giảm đau",
+    "Giảm ho Eugica",
+	"Khoáng chất (kẽm)"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Dị ứng",
+  "lessons": [
+    "Kháng viêm",
+	"Kháng H1",
+    "Giải độc gan",
+	"Vitamin (3B, C)"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Viêm mũi dị ứng",
+  "lessons": [
+    "Kháng viêm",
+	"Kháng H1",
+	"Vitamin (3B, C)"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Đau nhức xương khớp (có đau dạ dày)",
+  "lessons": [
+    "NSAID",
+    "Giãn cơ",
+	"Hạ sốt, giảm đau",	
+    "Vitamin (3B, C)",	
+    "PPI"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Đau nhức xương khớp",
+  "lessons": [
+    "NSAID",
+    "Giãn cơ",
+	"Hạ sốt, giảm đau",	
+    "Vitamin (3B, C)"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Cảm ho khan không nhiễm khuẩn",
+  "lessons": [
+    "Giảm ho - Dextromethorphan",
+    "Kháng H1",
+	"Hạ sốt, giảm đau",	
+    "Vitamin (3B, C)"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Đau dạ dày",
+  "lessons": [
+    "PPI",
+    "Domperidon",
+	"Giảm đau cơ trơn",	
+    "Gói antacid"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Tiêu chảy",
+  "lessons": [
+    "Loperamid",
+    "Giảm đau cơ trơn",
+	"Berberin",	
+    "Men tiêu hóa"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Rối loạn tiền đình",
+  "lessons": [
+    "Taginyl",
+    "Magnesium - B6",
+	"Cinarizin",	
+    "Hạ sốt, giảm đau"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Say tàu xe",
+  "lessons": [
+    "Vovina",
+    "Magnesium - B6",	
+    "Hạ sốt, giảm đau",
+	"Thuốc bổ"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Rối loạn tiêu hóa",
+  "lessons": [
+    "Domperidon",
+	"Giảm đau cơ trơn",		
+    "Men tiêu hóa"
+  ]
+},
+{
+  "group": "Người lớn",
+  "title": "Trĩ",
+  "lessons": [
+    "Rutin C",
+	"Agiosmin",		
+    "Hạ sốt, giảm đau"
+  ]
+},
+]  
